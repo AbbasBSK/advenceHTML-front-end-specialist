e880b8b2aa96166cd0268cf4c2cfcb9393472614
AbbasBSK <baqa4731@gmail.com>
2026-02-05 23:43:04 +0330

    after variable and nesting in SASS - video 2 in SASS topic

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const box = document.querySelector('.box');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+observer.observe(box); 
